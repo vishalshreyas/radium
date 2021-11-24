ec72757da205b2afa0110b9f1f441b0291e7e764
vishalshreyas <shreyasvishal@gmail.com>
2021-11-24 09:51:11 +0530

    json web token assignment

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://monty-python:SnYUEY4giV9rekw@functionup-backend-coho.0zpfv.mongodb.net/VishalShreyas_db?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://monty-python:SnYUEY4giV9rekw@functionup-backend-coho.0zpfv.mongodb.net/VishalShreyas_db?authSource=admin&replicaSet=atlas-60843q-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true")
     .then(() => console.log('mongodb running and connected'))
     .catch(err => console.log(err))
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,29 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const testController= require("../controllers/testController");
-const univMiddleware = require("../middlewares/univMiddleware")
+const userController= require("../controllers/bookController");
+const middleWare = require('../middlewares/univMiddlewares')
 
 
 
 
-//Assignment Middleware2
 
-router.get('/testMiddle', testController.testMiddle);
-router.post('/createProduct',testController.createProduct);     //productValidator
-router.post('/createUser',univMiddleware.isFreeAppvalidator,testController.createUser);     //userValidator
-router.post('/createOrder',univMiddleware.isFreeAppvalidator,testController.createOrder)
-// router.post('/purchaseOrder',univMiddleware.isFreeAppvalidator,testController.purchaseOrder);
-// purchase order
-// 
-//  If the isFreeApp header is true -->  1) the balance of the user is not deducted 
-//                                       2) and the amount in order is set to 0 
-//                                       3) as well the flag isFreeAppUser is set to true
-// If this header has a false value -->  1) the product’s price is checked
-//                                       2) This value is deducted from the user’s balance
-//                                       3) and the order amount is set to the product’s price
-//                                       4) as well as the flag isFreeAppUser is set to false in order document
+//Assignment
+
+router.post('/users', userController.registerUser);
+router.post('/login', userController.login);
+router.get('/users/:userId',middleWare.tokenCheck, userController.getDetails);
+router.put('/users/:userId',middleWare.tokenCheck,userController.updateDetails)
+
 
 
 module.exports = router;
-
