3a1e367480292aa07752120eaebf5d18149f2b54
vishalshreyas <shreyasvishal@gmail.com>
2021-12-14 11:56:36 +0530

    Updated Some of the Redis Part

--- src/controllers/urlcontroller.js
+++ src/controllers/urlcontroller.js
@@ -2,6 +2,32 @@ const urlModel = require("../models/urlShortModel");
 const shortId = require("shortid");
 const { response } = require("express");
 
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+  12845,
+  "redis-12845.c10.us-east-1-3.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("Xq0rqlRVXkNbnWcLZHDtABWmqMFxKhjv", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 const isValid = function (value) {
   if (typeof value === "undefined" || value === null) return false;
   if (typeof value === "string" && value.trim().length === 0) return false;
@@ -17,12 +43,10 @@ const shortenUrl = async function (req, res) {
     const requestBody = req.body;
 
     if (!isValidRequestBody(requestBody)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: "Invalid request parameters. Please provide URL details",
-        });
+      return res.status(400).send({
+        status: false,
+        message: "Invalid request parameters. Please provide URL details",
+      });
     }
 
     let longUrl = req.body.longUrl;
@@ -33,37 +57,29 @@ const shortenUrl = async function (req, res) {
         .send({ status: false, message: "Please provide Long URL" });
     }
 
-        
-        // const len = urlval.length       
-        // if (len > 1) {
-        //     return res.status(400).send({ status: false, msg: "URL cannot have space inbetween" });
-        // }
+    // const len = urlval.length
+    // if (len > 1) {
+    //     return res.status(400).send({ status: false, msg: "URL cannot have space inbetween" });
+    // }
 
-    if(!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(longUrl)){
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: "Invalid URL. Please provide correct URL",
-        });
+    if (
+      !/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(
+        longUrl
+      )
+    ) {
+      return res.status(400).send({
+        status: false,
+        message: "Invalid URL. Please provide correct URL",
+      });
     }
-
-      const urlTrim = longUrl.trim()
-        const urlval = urlTrim.split("").map(a => a.trim()).join("");
-      
-
-    const alreadyRegisteredUrlCode = await urlModel.findOne({
-      longUrl: urlval,
-    });
-
-    if (alreadyRegisteredUrlCode) {
-      return res
-        .status(201)
-        .send({
-          status: true,
-          data: alreadyRegisteredUrlCode,
-        });
+    if (!/(.com|.org|.co.in|.in|.co|.us)/.test(longUrl)) {
+      return res.send("Url is not valid");
     }
+    const urlTrim = longUrl.trim();
+    const urlval = urlTrim
+      .split("")
+      .map((a) => a.trim())
+      .join("");
 
     function makeid(length) {
       var result = "";
@@ -78,16 +94,29 @@ const shortenUrl = async function (req, res) {
     }
 
     let urlCode = makeid(9);
+    let checkUrlCode = await urlModel.findOne({ urlCode: urlCode });
     // const urlCode = shortId.generate();
+    if (checkUrlCode) {
+      urlCode = makeid(9);
+    }
 
     const shortUrl = `http://localhost:3000/${urlCode}`;
     const data = {};
     data["longUrl"] = urlval;
     data["shortUrl"] = shortUrl;
     data["urlCode"] = urlCode;
-    const response = await urlModel.create(data);
 
-    res.status(201).send({ status: true, data: response });
+    let cachedUrlData = await GET_ASYNC(`${data.longUrl}`);
+    if (cachedUrlData) {
+      res.send(cachedUrlData);
+    } else {
+      let profile = await urlModel.create(data);
+      await SET_ASYNC(`${data}`, JSON.stringify(profile));
+      res.status(201).send({ data: profile });
+    }
+    // const response = await urlModel.create(data);
+
+    // res.status(201).send({ status: true, data: response });
   } catch (err) {
     console.log(err.message);
     res.status(500).send({ msg: "Some error occured" });
@@ -98,19 +127,40 @@ const redirectUrl = async function (req, res) {
   try {
     const urlData = req.params.urlCode;
 
-    const urlDataTwo = urlData.split("").map(a => a.trim()).join("");
+    const urlDataTwo = urlData
+      .split("")
+      .map((a) => a.trim())
+      .join("");
 
-    const validData = await urlModel
-      .findOne({ urlCode: urlDataTwo })
-      .select({ longUrl: 1 });
+    let cachedUrlDataTwo = await GET_ASYNC(`${urlDataTwo}`);
 
-    if (!validData) {
-      return res
-        .status(400)
-        .send({ status: false, message: "Please provide a valid URL Code" });
+    if (cachedUrlDataTwo) {
+      res.redirect(307, cachedUrlDataTwo.longUrl);
+    } else {
+      let validData = await urlModel
+        .findOne({ urlCode: urlDataTwo })
+        .select({ longUrl: 1 });
+
+      if (!validData) {
+        return res
+          .status(400)
+          .send({ status: false, message: "Please provide a valid URL Code" });
+      }
+
+      await SET_ASYNC(`${validData}`, JSON.stringify(profile));
+      res.redirect(307, validData.longUrl);
     }
+    // const validData = await urlModel
+    //   .findOne({ urlCode: urlDataTwo })
+    //   .select({ longUrl: 1 });
 
-    res.redirect(307, validData.longUrl);
+    // if (!validData) {
+    //   return res
+    //     .status(400)
+    //     .send({ status: false, message: "Please provide a valid URL Code" });
+    // }
+
+    // res.redirect(307, validData.longUrl);
   } catch (err) {
     res.status(500).send({ status: false, message: err.message });
   }
