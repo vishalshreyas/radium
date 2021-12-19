9e0145f6a53e912e86fd6f0f77f27afe861ba532
vishalshreyas <shreyasvishal@gmail.com>
2021-12-19 17:16:47 +0530

    Submission of Task Tracker

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks]= useState([])
+  useEffect(() => {
+    const fetchTasks = async() => {
+      let headers = new Headers();
+      headers.append('Access-Control-Allow-Origin', 'http://localhost:5000/tasks');
+      headers.append('Access-Control-Allow-Credentials', 'true');
+
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      console.log(data)
+    }
+    fetchTasks()
+
+  },[])
+    //Add Task
+    const addTask = (task) =>{
+      const id = Math.floor(Math.random * 10000) + 1
+      const newTask = {id, ...task}
+      setTasks([...tasks, newTask])
+    }
+    //Delete Task
+    const deleteTask = (id) =>{
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+  
+    //Toggle Reminder
+    const toggleReminder = (id) =>{
+      setTasks(tasks.map((task) => task.id === id ? {...task, reminder:!task.reminder}: task))
+    }
+
+  return (
+    <div className="container">
+      <Header onAdd ={() => setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      { showAddTask && <AddTask onAdd = {addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks ={tasks} onDelete ={deleteTask} onToggle ={toggleReminder}/>):'No Tasks To Show'}
+    </div>
+  );
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const route = require('./routes/route.js');
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://monty-python:SnYUEY4giV9rekw@functionup-backend-coho.0zpfv.mongodb.net/VishalS_db?authSource=admin&replicaSet=atlas-60843q-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true")
-    .then(() => console.log('mongodb running and connected'))
-    .catch(err => console.log(err))
-
-    
-app.use('/', route);
-
-app.listen(process.env.PORT || 3000, function() {
-	console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
